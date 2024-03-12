1f85c0bcb8863a5b3a0f5de1724cc6dbd7b57227
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-03-12 15:04:49 -0300

    SYNC

--- globalAssets/js/nav.js
+++
@@ -1,6 +0,0 @@
-const btmn = document.querySelector('button#mnhamburg');
-const nav = document.querySelector('nav#menu');
-btmn.addEventListener('click', () => {
-    btmn.classList.toggle('open-nav');
-    nav.classList.toggle('open-nav');
-})
