d940095d639e80f156ef5c305efafb0c63259d05
william1234567891011121314 <williamboeira429@gmail.com>
2024-03-12 14:45:22 -0300

    main.js

---
+++ globalAssets/js/main.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body');
+body.innerHTML += `
+    <span id="nav">
+        <button id="mnhamburg">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <nav id="menu" class="closed-menu">
+            <div id="account">
+                <img src="/images/account-img" alt="Foto do seu perfil">
+                <p>Seu nome</p>
+            </div>
+            <a>sla</a>
+            <a>sla</a>
+            <a>sla</a>
+            <a>sla</a>
+        </nav>
+    </span>
+`
+const btmn = body.querySelector('button#mnhamburg');
+const nav = body.querySelector('nav#menu');
+btmn.addEventListener('click', () => {
+    btmn.classList.toggle('open-nav');
+    nav.classList.toggle('open-nav');
+})
