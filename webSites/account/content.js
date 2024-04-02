c4bd873b5da8ba7864aebef6cae646ee9bd716e1
william1234567891011121314 <williamboeira429@gmail.com>
2024-04-02 15:09:18 -0300

    account

---
+++ webSites/account/content.js
@@ -0,0 +1,9 @@
+const graphic = document.querySelector("div#graphic")
+var previousvacon = 0, cont = 0, xlayerunits = 20, ylayerunits = 10 , scale = 1000;
+
+graphic.style.gridTemplateColumns = `repeat(${xlayerunits}, 1fr)`;
+graphic.style.gridTemplateRows = `repeat(${ylayerunits}, 1fr)`;
+
+for(let i = 0; i < xlayerunits * ylayerunits; i++) {
+    graphic.innerHTML += "<span></span>";
+}
