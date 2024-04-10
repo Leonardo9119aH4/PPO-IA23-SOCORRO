4a292e60a029baaffdec39770679b5595d427ec1
Leonardo9119_aH4 <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-04-10 16:28:17 +0000

    XMLHtppRequest

--- webSites/quiz/content.js
+++ webSites/quiz/content.js
@@ -6,46 +6,47 @@ var score = 100 //pontuação sempre começa com 100
 var firstWrong = false //analisa se é o primeiro erro de resposta da questão
 var NAsk = 0 //número atual da questão
 
-async function main(){
-    const request = await fetch("test.json")
-    const quiz = await request.json()
-    function Asking(){
-        if(NAsk < quiz.length){
-            firstWrong = false
-            Ask.innerHTML = quiz[NAsk].ask
+const request = new XMLHttpRequest()
+request.open('GET', 'test.json', false)
+request.send()
+const quiz = JSON.parse(request.responseText)
 
-            AltAns.innerHTML = " "
-            for (let i=0; i<quiz[NAsk].alt.length; i++) {
-                AltAns.innerHTML += `<button>${quiz[NAsk].alt[i]}</button>`
-              }
-        }
-        else{
-            Win()
-        }
-    }
-    function Correct(){
-        NAsk++
-        Asking()
-    }
-    function Wrong(){
-        if(life>0){
-            if(firstWrong==false){ //cada questão só pode tirar 1 vida
-                firstWrong==true
-                life--
-                score -= 100/quiz.lenght //pontuação inicial dividido pela quantidade de questões
+function Asking(){
+    if(NAsk < quiz.length){
+        firstWrong = false
+        Ask.innerHTML = quiz[NAsk].ask
+
+        AltAns.innerHTML = " "
+        for (let i=0; i<quiz[NAsk].alt.length; i++) {
+            AltAns.innerHTML += `<button>${quiz[NAsk].alt[i]}</button>`
             }
-            Asking()
-        }
-        else{
-            GameOver()
-        }
     }
-    function GameOver(){
-
+    else{
+        Win()
     }
-    function Win(){
-        
-    }
+}
+function Correct(){
+    NAsk++
     Asking()
 }
-main()
+function Wrong(){
+    if(life>0){
+        if(firstWrong==false){ //cada questão só pode tirar 1 vida
+            firstWrong = true
+            life--
+            score -= 100/quiz.length //pontuação inicial dividido pela quantidade de questões
+        }
+        Asking()
+    }
+    else{
+        GameOver()
+    }
+}
+function GameOver(){
+
+}
+function Win(){
+    
+}
+Asking()
+
