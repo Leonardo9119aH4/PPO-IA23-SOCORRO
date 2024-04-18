43bd97b9c31e3476ab41b2a2415886d183bcb3bc
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-04-18 09:37:00 -0300

    correcao JS e JSON mestre

--- webSites/practice/content.js
+++ webSites/practice/content.js
@@ -1,43 +1,60 @@
 const Ask = document.querySelector("#ask>h1")
 const AltAns = document.querySelector("#response")
+const DocCSS = document.documentElement //constante para alterar CSS pelo JS
 
 var score = 100 //pontuação sempre começa com 100
 var firstWrong = false //analisa se é o primeiro erro de resposta da questão
 var NAsk = 0 //número atual da questão
 
-async function main(){
-    const request = await fetch("test.json")
-    const quiz = await request.json()
-    function Asking(){
-        if(NAsk < quiz.length){
-            firstWrong = false
-            Ask.innerHTML = quiz[NAsk].ask
+const request = new XMLHttpRequest()
+request.open('GET', '../../globalAssets/json/quiz/lv1.json', false) 
+request.send()
+const quiz = JSON.parse(request.responseText)
 
-            AltAns.innerHTML = " "
-            for (let i=0; i<quiz[NAsk].alt.length; i++) {
-                AltAns.innerHTML += `<button>${quiz[NAsk].alt[i]}</button>`
-              }
-        }
-        else{
-            Win()
-        }
-    }
-    function Correct(){
-        NAsk++
-        Asking()
-    }
-    function Wrong(){
-        if(life>0){
-            if(firstWrong==false){ //cada questão só pode tirar 1 vida
-                firstWrong==true
-                score -= 100/quiz.lenght //pontuação inicial dividido pela quantidade de questões
+function Asking(){
+    if(NAsk < quiz.length){
+        firstWrong = false
+        Ask.innerHTML = quiz[NAsk].ask
+
+        AltAns.innerHTML = " "
+        DocCSS.style.setProperty("--RepN", `${quiz[NAsk].alt.length}`)
+        for (let i=0; i<quiz[NAsk].alt.length; i++){
+            AltAns.innerHTML += `<button>${quiz[NAsk].alt[i]}</button>`
             }
-            Asking()
-        }
     }
-    function Win(){
-        
+    else{
+        Win()
     }
+}
+function Correct(){
+    NAsk++
     Asking()
 }
-main()
+function Wrong(){
+    if(life>0){
+        if(firstWrong==false){ //cada questão só pode tirar 1 vida
+            firstWrong = true
+            score -= 100/quiz.length //pontuação inicial dividido pela quantidade de questões
+        }
+        Asking()
+    }
+    else{
+        GameOver()
+    }
+}
+function Win(){
+    console.log("Ganhou!")
+}
+AltAns.addEventListener("click", ev =>{ //implementação para comparar alternativas (erro/acerto)
+    const AltC = ev.target 
+    const aAlt = [...AltAns.children]
+    const nAltC = aAlt.indexOf(AltC)
+    if(nAltC==quiz[NAsk].ans){
+        Correct()
+    }
+    else {
+        Wrong()
+    }
+})
+Asking()
+
