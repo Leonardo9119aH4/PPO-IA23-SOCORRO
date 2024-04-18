239523d1736d75fdbe1a41002a4c0bb271dc00d9
William12345678910111213 <williamboeira429@gmail.com>
2024-04-18 16:32:45 -0300

    import e export

--- globalAssets/js/main.js
+++ globalAssets/js/main.js
@@ -1,27 +1,28 @@
-const body = document.querySelector('body');
-body.innerHTML += `
-    <span id="nav">
-        <button id="mnhamburg">
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-        <nav id="menu" class="closed-menu">
-            <a href="/webSites/account/index.html" id="account">
-                <img src="/images/account-img" alt="Foto do seu perfil">
-                <p>Seu nome</p>
-            </a>
-            <a href="/webSites/main/index.html">Main page</a>
-            <a>sla</a>
-            <a>sla</a>
-            <a>sla</a>
-        </nav>
-    </span>
-`
-const btmn = body.querySelector('button#mnhamburg');
-const nav = body.querySelector('nav#menu');
-btmn.addEventListener('click', () => {
-    btmn.classList.toggle('open-nav');
-    nav.classList.toggle('open-nav');
-})
-
+export function main() {
+    const body = document.querySelector('body');
+    body.innerHTML += `
+        <span id="nav">
+            <button id="mnhamburg">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <nav id="menu" class="closed-menu">
+                <a href="/webSites/account/index.html" id="account">
+                    <img src="/images/account-img" alt="Foto do seu perfil">
+                    <p>Seu nome</p>
+                </a>
+                <a href="/webSites/main/index.html">Main page</a>
+                <a>sla</a>
+                <a>sla</a>
+                <a>sla</a>
+            </nav>
+        </span>
+    `
+    const btmn = body.querySelector('button#mnhamburg');
+    const nav = body.querySelector('nav#menu');
+    btmn.addEventListener('click', () => {
+        btmn.classList.toggle('open-nav');
+        nav.classList.toggle('open-nav');
+    })
+}
---
+++ webSites/main/content.js
@@ -0,0 +1,2 @@
+import {main} from '../../globalAssets/js/main.js'
+main()
