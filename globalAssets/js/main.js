19e126a59a4a1f03f92e017157e88cc88a56ae02
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-04-23 15:07:57 -0300

    DOR E SOFRIMENTO, ÓDIO DO JS

--- globalAssets/js/main.js
+++ globalAssets/js/main.js
@@ -1,15 +1,27 @@
 export function main(){
-    const body = document.querySelector('body');
-    fetch('http://localhost:3000/globalAssets/ejs/nav.ejs')
-    .then(response => response.text())
-    .then(html => {
-        body.innerHTML += html;
-    })    
-    const btmn = body.querySelector('button#mnhamburg');
-    const nav = body.querySelector('nav#menu');
-    btmn.addEventListener('click', () => {
-        btmn.classList.toggle('open-nav');
-        nav.classList.toggle('open-nav');
-    })
-}
+    const nav = document.querySelector('nav') //referenciar o nav para injeção de html
+    function postDOM(){ //função para funcionamento dos botões, tá gambiarra? tá. tudo culpa do js
+        const btmn = nav.querySelector('button#mnhamburg')
+        const subNav = nav.querySelector('div#subNav')
+        btmn.addEventListener('click', () => {
+            btmn.classList.toggle('open-nav')
+            subNav.classList.toggle('open-nav')
+        })
+    }
 
+    const mainRqst = new XMLHttpRequest()
+    mainRqst.open("get", 'http://localhost:3000/globalAssets/ejs/nav.ejs', true) //a injeção do html será feita pelo ejs
+    mainRqst.onreadystatechange = function() {
+        if (mainRqst.readyState === XMLHttpRequest.DONE) {
+            if (mainRqst.status === 200) {
+                nav.innerHTML += mainRqst.responseText
+                postDOM() //gambiara para aquele código só ser executado depois da injeção deo ejs no dom, pq a me### do await fetch quebra o código e pelo XMLHTTPRequest o js não espera a injeção para executar o resto
+            } else {
+                console.error('Erro ao carregar o arquivo:', mainRqst.status)
+            }
+        }
+    };
+    mainRqst.send()
+    
+}
+//te odeio javascript
