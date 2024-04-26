7e34d4b6e58fffb087859da1d95f77f8150d6734
William1234567891011121314 <williamboeira429@gmail.com>
2024-04-26 17:01:27 -0300

    Condicionais prontas

--- webSites/rpg/content.js
+++ webSites/rpg/content.js
@@ -1,27 +1,32 @@
 import {main} from 'http://localhost:3000/globalAssets/js/main.js'
-main()
+
 import { movecalc } from 'http://localhost:3000/webSites/rpg/move.js'
 import { conditional } from 'http://localhost:3000/webSites/rpg/conditional.js'
 
 const button = document.querySelector('button#exec')
 const input = document.querySelector('div#code_input>textarea')
 
-async function move() {
+async function load() {
     const requestcommand = await fetch('http://localhost:3000/webSites/rpg/localassets/commands.json')
     const commandsjson = await requestcommand.json()
     const inputcommands = input.value.split('\n')
+    let condition = false
     for(let i = 0; i < inputcommands.length; i++) {
-        let condition = false
         let inputsplit = inputcommands[i].split('')
         inputsplit.forEach(el => {
             if(el == '{') {
-                i = conditional(inputsplit, inputcommands, i)
+                i = conditional(inputsplit, inputcommands, i, commandsjson)
                 condition = true
             }
+            if(el == '}') {
+                condition = false
+            }
         });
-        if(condition){
+        console.log(condition)
+        if(!condition){
             commandsjson.forEach(commandelement => {
                 if(inputcommands[i] == commandelement.command) { //se o input for igual a algum comando do json executa o código
+                    console.log(inputcommands[i])
                     movecalc(commandelement)
                 }
             })
@@ -30,5 +35,6 @@ async function move() {
 }
 
 button.addEventListener('click', () => {
-    move()
+    console.log('botao')
+    load()
 })
