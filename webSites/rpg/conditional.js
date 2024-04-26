bab0e91864b7dbb610f9f96a2fd72f4989b8c0ac
William1234567891011121314 <williamboeira429@gmail.com>
2024-04-26 20:15:00 -0300

    condicional e feedback

--- webSites/rpg/conditional.js
+++ webSites/rpg/conditional.js
@@ -1,6 +1,5 @@
 import { movecalc } from "http://localhost:3000/webSites/rpg/move.js";
 export function conditional(input, inputcommands, actualline, commandlist) {
-    console.log('condicional')
     let condition = false
     let expression = []
     input.forEach(el => {
@@ -13,25 +12,20 @@ export function conditional(input, inputcommands, actualline, commandlist) {
     });
     expression.splice(0, 1)
     expression = expression.join('')
-    console.log(expression)
     if(eval(expression)) {
         let commands = inputcommands
         let lines = inputcommands.length
-        console.log(commands)
         commands.splice(0, actualline)
-        console.log(commands)
         commands.forEach((el, index) => {
             if(el == '}') {
                 commands.splice(index, lines - index)
                 lines = index
-                console.log('caiu na condicional')
             }
         });
         commands.shift()
         commands.forEach(el => {
             commandlist.forEach(commandelement => {
                 if(el == commandelement.command) { //se o input for igual a algum comando do json executa o código
-                    console.log(commandelement)
                     movecalc(commandelement)
                 }
             });
