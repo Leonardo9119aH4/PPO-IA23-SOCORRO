32ab60838905585238d5ec76b38bdb13b2b49dbb
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-05-01 16:29:40 -0300

    inicio blocks

--- webSites/blocks/content.js
+++ webSites/blocks/content.js
@@ -1,2 +1,13 @@
 import {main} from "http://localhost:3000/globalAssets/js/main.js"
-main()
+
+const aside = document.querySelector("aside")
+const header = document.querySelector("header")
+const codeBlocks = document.querySelector("main")
+
+async function content(){
+    main()
+    const asideRqst = await fetch("http://localhost:3000/webSites/blocks/localAssets/levels/lv1/aside.ejs")
+    const asideEJS = await asideRqst.text()
+    aside.innerHTML = asideEJS
+}
+content()
