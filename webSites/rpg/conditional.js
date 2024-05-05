4caa077dc1ba643cbf91409799fcba99bc225c49
William1234567891011121314 <williamboeira429@gmail.com>
2024-05-05 16:32:31 -0300

    colisão corrigida

--- webSites/rpg/conditional.js
+++ webSites/rpg/conditional.js
@@ -13,18 +13,18 @@ export function conditional(input, inputcommands, actualline, commandlist, vars)
     expression.splice(0, 1)
     expression = expression.join('')
     console.log(expression)
+    let commands = inputcommands
+    let lines = inputcommands.length
+    commands.splice(0, actualline)
+    commands.forEach((el, index) => {
+        if(el == '}') {
+            commands.splice(index, lines - index)
+            lines = index
+        }
+    });
     if(eval(expression)) {
-        let commands = inputcommands
-        let lines = inputcommands.length
-        commands.splice(0, actualline)
-        commands.forEach((el, index) => {
-            if(el == '}') {
-                commands.splice(index, lines - index)
-                lines = index
-            }
-        });
         commands.shift()
-        console.log(commands)
+        console.log(lines)
         commands.forEach(el => {
             commandlist.forEach(commandelement => {
                 if(el == commandelement.command) { //se o input for igual a algum comando do json executa o código
@@ -35,6 +35,6 @@ export function conditional(input, inputcommands, actualline, commandlist, vars)
         return lines
     } else {
         console.log('Não é uma condicional')
-        return actualline
+        return lines
     }
 }
