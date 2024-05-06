c5738e9d6845d761bab8bc31e7ebc43b7860959a
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-05-06 11:04:27 -0300

    desenvolvimento blocks

--- webSites/blocks/content.js
+++ webSites/blocks/content.js
@@ -2,23 +2,35 @@ import {main} from "http://localhost:3000/globalAssets/js/main.js"
 import "http://localhost:3000/globalAssets/js/jquery.js"
 import "http://localhost:3000/globalAssets/js/jquery-ui.js"
 
-const aside = document.querySelector("aside")
-const header = document.querySelector("header")
-const codeBlocks = document.querySelector("main")
-const title =  document.querySelector("title")
+const aside = document.querySelector("aside") //local dos blocos arrastáeis
+const header = document.querySelector("header") //cabeçario
+const codeBlocks = document.querySelector("main") //onde o scratch fica
+const title =  document.querySelector("title") //título
 
 async function content(){
     main()
     var level = 1 //TEMPORÁRIO! Futura ligação com banco de dados (não estou copiando o comentário do William)
     const masterRqst = await fetch("http://localhost:3000/globalAssets/json/master.json")
-    const master = await masterRqst.json()
+    const master = await masterRqst.json() //json mestre
     title.innerHTML = master[level].level_title
     header.innerHTML = master[level].level_header
     const asideRqst = await fetch(`http://localhost:3000/webSites/blocks/localAssets/levels/lv${level}/aside.ejs`)
-    const asideEJS = await asideRqst.text()
+    const asideEJS = await asideRqst.text() //blocos laterais
+    const blocksRqst =  await fetch(`http://localhost:3000/webSites/blocks/localAssets/levels/lv${level}/code.ejs`)
+    const blocksEJS = await blocksRqst.text()
+    codeBlocks.innerHTML = blocksEJS
     aside.innerHTML = asideEJS
+    const blocksID = document.querySelectorAll(".blocks")
+    const reBlID = document.querySelectorAll(".reBl")
+    console.log(blocksID)
+    console.log(reBlID)
     $(document).ready(function() {
-        $('.blocks').draggable()
+        $(".blocks").draggable()
+        $(".reBl").droppable({
+            drop: function(event, ui){
+                
+            }
+        })
     })
 }
 content()
