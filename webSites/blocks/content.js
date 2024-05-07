5bd66a407adc93340a24c715c3eb2ee0898ea6f1
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-05-07 09:05:42 -0300

    logica de encaixe scratch

--- webSites/blocks/content.js
+++ webSites/blocks/content.js
@@ -1,29 +1,30 @@
 import {main} from "http://localhost:3000/globalAssets/js/main.js"
 import "http://localhost:3000/globalAssets/js/jquery.js"
 import "http://localhost:3000/globalAssets/js/jquery-ui.js"
-
 const aside = document.querySelector("aside") //local dos blocos arrastáeis
 const header = document.querySelector("header") //cabeçario
 const codeBlocks = document.querySelector("main") //onde o scratch fica
 const title =  document.querySelector("title") //título
+var level = 1 //TEMPORÁRIO! Futura ligação com banco de dados (não estou copiando o comentário do William)
 
-async function content(){
-    main()
-    var level = 1 //TEMPORÁRIO! Futura ligação com banco de dados (não estou copiando o comentário do William)
-    const masterRqst = await fetch("http://localhost:3000/globalAssets/json/master.json")
-    const master = await masterRqst.json() //json mestre
-    title.innerHTML = master[level].level_title
-    header.innerHTML = master[level].level_header
+async function loadDOM(){
     const asideRqst = await fetch(`http://localhost:3000/webSites/blocks/localAssets/levels/lv${level}/aside.ejs`)
     const asideEJS = await asideRqst.text() //blocos laterais
     const blocksRqst =  await fetch(`http://localhost:3000/webSites/blocks/localAssets/levels/lv${level}/code.ejs`)
     const blocksEJS = await blocksRqst.text()
     codeBlocks.innerHTML = blocksEJS
     aside.innerHTML = asideEJS
-    const blocksID = document.querySelectorAll(".blocks")
-    const reBlID = document.querySelectorAll(".reBl")
-    console.log(blocksID)
-    console.log(reBlID)
+}
+
+async function content(){
+    main()
+    await loadDOM()
+    const masterRqst = await fetch("http://localhost:3000/globalAssets/json/master.json")
+    const master = await masterRqst.json() //json mestre
+    title.innerHTML = master[level].level_title
+    header.innerHTML = master[level].level_header
+    const blocksRef = document.querySelectorAll(".blocks")
+    const reBlRef = document.querySelectorAll(".reBl")
     $(document).ready(function() {
         $(".blocks").draggable()
         $(".reBl").droppable({
@@ -32,5 +33,16 @@ async function content(){
             }
         })
     })
+    blocksRef.forEach(blockRef => {
+        blockRef.addEventListener("dragend", ev => {
+            const blockId = ev.target.id
+            const gapId = `gap${blockId.slice(2)}`
+            console.log("foi")
+            if(blockId === gapId){
+                console.log(`O bloco ${blockId} está encaixado corretamente na lacuna ${gapId}.`)
+            }
+        })
+    })
+    
 }
 content()
