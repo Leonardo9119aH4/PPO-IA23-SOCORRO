0bb302157d7bedaafdb6cb2106fc3d9afc32d18d
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-05-13 13:49:24 -0300

    detecção de correspondência dos blocos

--- webSites/blocks/localAssets/dragAndDrop.js
+++ webSites/blocks/localAssets/dragAndDrop.js
@@ -25,8 +25,37 @@ export class dragBlock {
         });
     }
 
-    destroy() {
+    destroy(){
         // Adicione a lógica de destruição aqui
     }
 }
+export class receiveBlock{
+    constructor(element, id) {
+        this.element = element
+        this.id = id
+        this.reBlElements = []
+    }
+    addReBlElement(element) {
+        this.reBlElements.push(element);
+    }
+    checkCollision(dragBlock) {
+        const rect1 = this.element.getBoundingClientRect()
+        const rect2 = dragBlock.getBoundingClientRect()
+        if (rect1.left < rect2.right && rect1.right > rect2.left &&
+            rect1.top < rect2.bottom && rect1.bottom > rect2.top) {
+            // Sobreposição detectada
+            console.log(`Sobreposição com gap${this.id} detectada`)
+        }
+        this.reBlElements.forEach(reBlElement => {
+            const reBlRect = reBlElement.getBoundingClientRect();
+            if (reBlRect.left < rect2.right && reBlRect.right > rect2.left &&
+                reBlRect.top < rect2.bottom && reBlRect.bottom > rect2.top) {
+                // Sobreposição com reBl detectada
+                console.log(`Sobreposição com reBl${this.id} detectada`);
+            }
+        });
+    }
+    destroy(){
 
+    }
+}
