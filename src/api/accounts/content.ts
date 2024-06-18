eee59ca832fcdb7738a9fcc82bd3798da480e012
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-06-18 14:49:08 -0300

    sync 2

--- src/api/accounts/content.ts
+++ src/api/accounts/content.ts
@@ -2,9 +2,14 @@ import {Application, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 export async function signUp(app: Application, prisma: PrismaClient){
     app.post("/api/signup", (req: Request, res: Response)=>{
+        let statuscode: number = 0 // 1 -> emails não batem, 2 -> senhas não batem, 4 -> já existe um usuário com esse username. Se o statuscode for 0, é tudo OK, se não, somar a tabela e verificar os erros
         let info = req.body.json()
-        if(info.login=="qywt"){
-            res.status(200).json("foi")
+        if(! info.email == info.confirmEmail){ 
+            statuscode += 1
         }
+        if(! info.password == info.confirmPassword){
+            statuscode +=2
+        }
+        //
     })
 }
