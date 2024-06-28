489be0be3fda3adc254f3b7d8ea3112c304bc439
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-06-28 13:58:02 -0300

    sistema login cadastro

--- src/api/map.ts
+++ src/api/map.ts
@@ -1,8 +1,9 @@
 import { Application } from "express"
 import { PrismaClient } from "@prisma/client"
 import {howLevel} from "./quiz/levelRqst"
-import {signUp} from "./accounts/content"
+import {signUp, signIn} from "./accounts/content"
 export async function executeAll(app: Application, prisma: PrismaClient){
     howLevel()
     signUp(app, prisma)
+    signIn(app, prisma)
 }
