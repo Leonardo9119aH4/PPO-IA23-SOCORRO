10a173cab0fd679b92cc31b1002a8c0b09fbc63b
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-07-02 15:08:18 -0300

    backend e niveis

--- src/api/map.ts
+++ src/api/map.ts
@@ -1,9 +1,12 @@
 import { Application } from "express"
 import { PrismaClient } from "@prisma/client"
-import {howLevel} from "./quiz/levelRqst"
-import {signUp, signIn} from "./accounts/content"
+import {howLevel} from "./levels/levelRqst"
+import {signUp} from "./accounts/signup"
+import {signIn} from "./accounts/signin"
+import {logAuth} from "./accounts/logAuth"
 export async function executeAll(app: Application, prisma: PrismaClient){
-    howLevel()
+    howLevel(app, prisma)
     signUp(app, prisma)
     signIn(app, prisma)
+    logAuth(app, prisma)
 }
