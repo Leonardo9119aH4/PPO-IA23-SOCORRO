860fe573146d5264fdfed6c927f1b86f0b71c674
William1234567891011121314 <williamboeira429@gmail.com>
2024-07-09 17:06:33 -0300

    login inicio

--- src/webSites/login/content.js
+++ src/webSites/login/content.js
@@ -1,2 +1,10 @@
 import {main} from 'http://localhost:3000/globalAssets/js/main.js'
 main()
+
+async function passver() {
+    
+}
+
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+
