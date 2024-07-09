1072f30a846bf0b8fe35c3bb277e17d98c2627e3
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-07-09 18:06:38 +0000

    requisição de níevl

--- src/webSites/levels/content.js
+++ src/webSites/levels/content.js
@@ -8,16 +8,16 @@ async function content(){
     const master = await masterRqst.json();
     for(let i=0; i<master.length; i++){ //injeta os níveis
         if(master[i].type == "intro"){
-            mainTag.innerHTML += `<div class='intro' id=N${i}'>${i}</div>`
+            mainTag.innerHTML += `<div class='intro' id=${i}'>${i}</div>`
         }
         if(master[i].type == "quiz"){
-            mainTag.innerHTML += `<div class='quiz' id=N${i}'>${i}</div>`
+            mainTag.innerHTML += `<div class='quiz' id=${i}'>${i}</div>`
         }
         if(master[i].type == "rpg"){
-            mainTag.innerHTML += `<div class='rpg' id=N${i}'>${i}</div>`
+            mainTag.innerHTML += `<div class='rpg' id=${i}'>${i}</div>`
         }
         if(master[i].type == "blocks"){
-            mainTag.innerHTML += `<div class='blocks' id=N${i}'>${i}</div>`
+            mainTag.innerHTML += `<div class='blocks' id=${i}'>${i}</div>`
         } 
     }
     const intro = mainTag.querySelectorAll(".intro")
@@ -26,29 +26,22 @@ async function content(){
     const blocks = mainTag.querySelectorAll(".blocks")
     intro.forEach(el=>{
         el.addEventListener("click", async ev=>{
-            let level = await fetch("/api/private/levelrqst", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify({level: el.id})
-            })
-            window.location.href = "/webSites/intro/index.html"
+            window.location.href = "/webSites/intro/index.html?level=" + el.id
         })
     })
     quiz.forEach(el=>{
         el.addEventListener("click", ev=>{
-            window.location.href = "/webSites/quiz/index.html"
+            window.location.href = "/webSites/quiz/index.html?level=" + el.id
         })
     })
     rpg.forEach(el=>{
         el.addEventListener("click", ev=>{
-            window.location.href = "/webSites/rpg/index.html"
+            window.location.href = "/webSites/rpg/index.html?level=" + el.id
         })
     })
     blocks.forEach(el=>{
         el.addEventListener("click", ev=>{
-            window.location.href = "/webSites/blocks/index.html"
+            window.location.href = "/webSites/blocks/index.html?level=" + el.id
         })
     })
 }
