0c8ebf023b5b452a2b004b6ac0bb02d4681f3b1a
William1234567891011121314 <williamboeira429@gmail.com>
2024-07-11 09:54:36 -0300

    login

--- src/api/rpg/content.ts
+++ src/api/rpg/content.ts
@@ -4,6 +4,34 @@ function run(app: Application){
     app.post('/move', (req: Request, res: Response) => {
         let inputcommands = req.body.json();
         var gameVars = [new Array(0), new Array(0), new Array(0)]
-        //parei aqui
+        
     })
+}
+
+function load(inputcommands: Array<string>, commandsjson: Array<string>, gameVars: Array<Array<string>>, GameDOM: Object) {
+    for(let i = 0; i < inputcommands.length; i++) {
+        console.log(inputcommands)
+        let inputsplit = inputcommands[i].split('')
+        setVars(inputcommands[i], inputsplit, gameVars)
+        console.log(inputcommands)
+        console.log(gameVars, i)
+        console.log(GameDOM)
+        console.log(i)
+        let varinputcommand = inputcommands[i]
+        inputcommands[i] = getVars(inputcommands[i], inputsplit, gameVars)
+        inputsplit = inputcommands[i].split('')
+        if(detectLoop(inputcommands[i])){
+             console.log('temloop')
+             i = loadLoop(varinputcommand, inputcommands, i, commandsjson, gameVars, GameDOM)
+        }
+        if(inputcommands[i].indexOf('se ') != -1 || inputcommands[i].indexOf('se(') != -1) {
+            console.log('tem condicional')
+            i = conditional(inputsplit, inputcommands, i, commandsjson, gameVars, GameDOM)
+        }
+        commandsjson.forEach(commandelement => {
+            if(inputcommands[i] == commandelement.command) { //se o input for igual a algum comando do json executa o código
+                movecalc(commandelement, GameDOM)
+            }
+        })
+    }
 }
