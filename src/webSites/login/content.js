d6f2881afe6d84a67bf649198f63a0e46512751a
William1234567891011121314 <williamboeira429@gmail.com>
2024-07-12 14:27:56 -0300

    request ok

--- src/webSites/login/content.js
+++ src/webSites/login/content.js
@@ -2,28 +2,45 @@ import {main} from '/globalAssets/js/main.js'
 main()
 
 let email = document.querySelector('#email')
-let paswword = document.querySelector("#password")
+let password = document.querySelector("#password")
 
-let btComfirm = document.querySelector("#btComfirm")
+let btConfirm = document.querySelector("#btConfirm")
 
-let userCheck = document.querySelector("metodouser")
+let userCheck = document.querySelector("#metodouser")
 let emailCheck = document.querySelector("#metodoemail")
+let telefone = document.querySelector("#metodotelefone")
 
 let status
 
-btComfirm.onclick = async function() {
+btConfirm.onclick = async function() {
     if(userCheck.checked){
         status = 1
     } else if (emailCheck.checked){
         status = 2
-    } else {
+    } else if (telefone.checked){
         status = 3
+    } else {
+        alert("Selecione um método!")
     }
-    let response = await fetch("/api/signin", {
+    let response = await fetch('/api/signin', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
-        body: {credential: status}
+        body: JSON.stringify({credential: status, login: email.value, password: password.value})
     })
+    switch(response){
+        case -1:
+            alert("Há um campo vazio!")
+            break
+        case -2:
+            alert("usuário não encontrado")
+            break
+        case 1:
+            window.location.href = "/webSites/main/index.html"
+            break
+        case 500:
+            alert("Erro interno do servidor!")
+            break
+    }
 }
