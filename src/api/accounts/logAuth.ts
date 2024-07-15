52998b7bee4894a7a36329755435200f91815a03
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-07-15 20:48:50 -0300

    cookies

--- src/api/accounts/logAuth.ts
+++ src/api/accounts/logAuth.ts
@@ -1,9 +1,20 @@
 import {Application, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
+import cookieParser from 'cookie-parser'
 export async function logAuth(app: Application, prisma: PrismaClient){
-    app.post("/api/private/logauth", async (req: Request, res: Response)=>{
+    app.post("/api/private/logauth", async (req: Request, res: Response)=>{ //valida o cookie enviado pelo cliente
         try{
-            
+            const authKeys = await prisma.authKey.findMany({
+                select: {
+                    key: true,
+                    userId: true,
+                }
+            })
+            authKeys.forEach(async el =>{
+                if(req.cookies["authKey"] === el.key){
+                    //parei aqui
+                }
+            })
         }
         catch{
             res.status(500)
