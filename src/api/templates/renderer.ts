3ea84988f5977e030406722a46ace3f3c119fabf
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-07-17 14:36:23 -0300

    sync 1

--- src/api/templates/renderer.ts
+++ src/api/templates/renderer.ts
@@ -2,13 +2,16 @@ import { Application, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import ejs from "ejs";
 import { logAuth } from "../accounts/cookies";
-export async function renderer(app: Application, prisma: PrismaClient){
-    app.post("/api/getNav", async (req, res) => {
+export async function rendererNav(app: Application, prisma: PrismaClient){
+    console.log("1")
+    app.post("/api/getnav", async (req: Request, res: Response)=>{
+        console.log("2")
         try{
+            console.log("3")
             const userId: number = await logAuth(prisma, req)
             let html
             if(userId === -1){
-                html = ejs.renderFile("./nav.ejs", {username: null, logged: false})
+                html = await ejs.renderFile("./nav.ejs", {username: null, logged: false})
                 res.status(200).json(html)
             }
             else if(isNaN(userId)){
@@ -27,6 +30,5 @@ export async function renderer(app: Application, prisma: PrismaClient){
         catch{
             res.sendStatus(500)
         }
-        
     })
 }
