ffc4e575748d7ce5c0467ff8f0480487f7cdf816
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-07-17 11:33:39 -0300

    renderização ejs

--- src/globalAssets/js/main.js
+++ src/globalAssets/js/main.js
@@ -5,12 +5,28 @@ export async function main(){
     nav.innerHTML = mainEJS
     const btmn = nav.querySelector('button#mnhamburg')
     const subNav = nav.querySelector('div#subNav')
+    const divUsername = nav.querySelector("p#username")
     btmn.addEventListener('click', () => {
         btmn.classList.toggle('open-nav')
         subNav.classList.toggle('open-nav')
     })
+    let username = await fetch("/api/private/username", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"action": "get"})
+    })
+    if(username.status === 500 || username.status === 502){
+        fatalError(username.status)
+    }
+    else if(username.status === 401){
+        divUsername.innerHTML = ""
+    }
+    else{
+        divUsername.innerHTML = await username.json()
+    }
 }
-export async function fatalError(error){
+export async function fatalError(error){ //função caso de erro fudido no servidor
     document.body.innerHTML = `<h1>Erro interno ${error} no servidor. Tente novamente mais tarde</h1>`
 }
-//te odeio javascript
