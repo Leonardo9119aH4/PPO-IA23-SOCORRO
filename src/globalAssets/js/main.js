0ea9923913d98e448abe17fb6e413ba075d7f262
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-07-17 14:36:23 -0300

    sync 1

--- src/globalAssets/js/main.js
+++ src/globalAssets/js/main.js
@@ -1,6 +1,6 @@
 export async function main(){
     const nav = document.querySelector('nav') //referenciar o nav para injeção de html
-    const mainRqst = await fetch("/globalAssets/ejs/nav.ejs")
+    const mainRqst = await fetch("/api/getnav")
     const mainEJS = await mainRqst.text()
     nav.innerHTML = mainEJS
     const btmn = nav.querySelector('button#mnhamburg')
@@ -10,22 +10,6 @@ export async function main(){
         btmn.classList.toggle('open-nav')
         subNav.classList.toggle('open-nav')
     })
-    let username = await fetch("/api/private/username", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({"action": "get"})
-    })
-    if(username.status === 500 || username.status === 502){
-        fatalError(username.status)
-    }
-    else if(username.status === 401){
-        divUsername.innerHTML = ""
-    }
-    else{
-        divUsername.innerHTML = await username.json()
-    }
 }
 export async function fatalError(error){ //função caso de erro fudido no servidor
     document.body.innerHTML = `<h1>Erro interno ${error} no servidor. Tente novamente mais tarde</h1>`
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const prisma = new PrismaClient()
 async function main(){
   app.use(bodyParser.json())
   app.use(cookieParser())
+  app.use(express.urlencoded({ extended: true }))
   app.use('/webSites', express.static(path.join(__dirname, 'webSites'))) // Mapeia as respectivas pastas para o FrontEnd acessar
   app.use('/globalAssets', express.static(path.join(__dirname, 'globalAssets')))
   app.get('/', (req: Request, res: Response) => { // Redireciona o "/" para "/webSites/main/index.html"
