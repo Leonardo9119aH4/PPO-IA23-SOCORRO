246c08efec4810e9ac140c2b830b70c693101db1
William1234567891011121314 <williamboeira429@gmail.com>
2024-07-17 21:12:25 -0300

    Exclusão do frontend do rpg

--- src/webSites/rpg/win.js
+++
@@ -1,9 +0,0 @@
-export async function win() {
-    // var ejsrequest = await (await fetch('http://localhost:3000/webSites/rpg/localassets/feedback/win.ejs')).text()
-    // document.querySelector('body').innerHTML += ejsrequest
-    // document.querySelector('button#nextlevel').onclick = function() {
-    //     lv++
-    //     window.location.href = 'http://localhost:3000/webSites/levels/index.html'
-    // }
-    console.log('Você ganhou!')
-}
