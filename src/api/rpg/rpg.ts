ec05abbcba598c2477f9eb050664c0b067cf5a8f
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-07-31 16:28:39 +0000

    rpg

--- src/api/rpg/rpg.ts
+++ src/api/rpg/rpg.ts
@@ -3,7 +3,7 @@ import { Application, Request, Response } from "express";
 import path from "path";
 import { logAuth } from "../accounts/cookies";
 
-export async function getRpg(app: Application, prisma: PrismaClient){
+export async function rpg(app: Application, prisma: PrismaClient){
     app.post("/api/private/getrpg", async (req: Request, res: Response)=>{
         try{
             let userId: number = await logAuth(prisma, req)
@@ -15,32 +15,12 @@ export async function getRpg(app: Application, prisma: PrismaClient){
             }
             else{
                 const user = await prisma.user.findUnique({
-                    where: {
+                    select: {
                         id: userId
                     }
                 })
-                if(user != null && req.body.level > user.level){ 
+                if(req.body.level > user.level){
                     res.sendStatus(403)
-                }
-                else{
-                    try{
-                        if(req.body.getfile === 0){
-                            res.sendFile(path.join(__dirname, `./levels/lv${req.body.level}/config.json`)) //configuração das cenas
-                        }
-                        else if(req.body.getfile === 1){
-                            res.sendFile(path.join(__dirname, `./levels/lv${req.body.level}/load.mjs`)) //carregador do nível
-                        }
-                        else if(req.body.getfile === 2){
-                            res.sendFile(path.join(__dirname, `./levels/lv${req.body.level}/level.mjs`)) //o nível em si
-                        }
-                        else{
-                            res.sendStatus(400) //não tem o que enviar
-                        }
-                            
-                    }
-                    catch{
-                        res.sendStatus(404)
-                    }
                 }
             }
         }
