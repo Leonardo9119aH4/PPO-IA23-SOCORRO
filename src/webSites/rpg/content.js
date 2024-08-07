3952ad7f836959853359426f36b9297b938ecd39
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-07 16:21:48 +0000

    rpg SOCORRO!

--- src/webSites/rpg/content.js
+++ src/webSites/rpg/content.js
@@ -74,7 +74,7 @@ async function content(){
     const master = data1[1]
     const life = data1[2]
     const data2 = await getLevel(level) //arquivos secundários, precisam dos arquivos primários
-    let config = data2[0]
+    // let config = data2[0]
     const loadText = data2[1]
     const levelText = data2[2]
     const loadBlob = new Blob([loadText], { type: 'application/javascript' });
@@ -85,11 +85,13 @@ async function content(){
     const levelScript = await import(levelUrl)
     class LoadLevel extends loadScript.LoadLevel{}
     class Level extends levelScript.Level{}
-    config = { //gambiarra pra receber Phaser.AUTO e não uma string, js e json tem que ser superados
-        ...config,
-        type: Phaser.AUTO
+    const config = { //gambiarra pq o js é burro e não consegue obter isso com json
+        width: 800,
+        height: 600,
+        parent: 'game-container',
+        scene: [LoadLevel, Level], 
+        type: Phaser.AUTO 
     };
-    console.log(config)
     const game = new Phaser.Game(config)
 }
 main()
