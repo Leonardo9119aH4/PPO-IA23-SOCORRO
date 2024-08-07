2b7cf1e68fa672bef72d0216098d6efa947227a2
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-07 16:21:48 +0000

    rpg SOCORRO!

--- src/api/rpg/levels/lv5/load.mjs
+++ src/api/rpg/levels/lv5/load.mjs
@@ -4,19 +4,22 @@ export class LoadLevel extends Phaser.Scene {
     }
     preload(){
         this.load.image("scenario", "/webSites/rpg/localAssets/scenario.png")
-        this.load.image("wall", "/webSites/rpg/localAssets/wall.png")
-        this.load.spritesheet("ciceroIdle", "/webSites/rpg/localAssets/sprites/Cicero/CiceroIdle.png")
-        his.load.spritesheet("ciceroWalk", "/webSites/rpg/localAssets/sprites/Cicero/CiceroWalk.png")
-        his.load.spritesheet("ciceroAttackDown", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackDown.png")
-        his.load.spritesheet("ciceroAttackUp", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackUp.png")
-        his.load.spritesheet("ciceroAttackSides", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackSides.png")
-        this.load.spritesheet("bug1Idle", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1Idle.png")
-        this.load.spritesheet("bug1AttackSides", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1AttackSides.png")
-        this.load.spritesheet("bug2Idle", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2Idle.png")
-        this.load.spritesheet("bug2AttackSides", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2AttackSides.png")
-        this.load.spritesheet("bug3Idle", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3Idle.png")
-        this.load.spritesheet("bug3AttackSides", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3AttackSides.png")
-        this.load.music("main-music", "/webSites/rpg/localAssets/music.mp3")
+        //this.load.image("wall", "/webSites/rpg/localAssets/wall.png")
+        this.load.spritesheet("ciceroIdle", "/webSites/rpg/localAssets/sprites/Cicero/CiceroIdle.png",{
+            frameWidth: 17*50,
+            frameHeight: 40*50
+        })
+        this.load.spritesheet("ciceroWalk", "/webSites/rpg/localAssets/sprites/Cicero/CiceroWalk.png")
+        this.load.spritesheet("ciceroAttackDown", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackDown.png")
+        this.load.spritesheet("ciceroAttackUp", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackUp.png")
+        this.load.spritesheet("ciceroAttackSides", "/webSites/rpg/localAssets/sprites/Cicero/CiceroAttackSides.png")
+        // this.load.spritesheet("bug1Idle", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1Idle.png")
+        // this.load.spritesheet("bug1AttackSides", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1AttackSides.png")
+        // this.load.spritesheet("bug2Idle", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2Idle.png")
+        // this.load.spritesheet("bug2AttackSides", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2AttackSides.png")
+        // this.load.spritesheet("bug3Idle", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3Idle.png")
+        // this.load.spritesheet("bug3AttackSides", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3AttackSides.png")
+        // this.load.music("main-music", "/webSites/rpg/localAssets/music.mp3")
     }
     create(){
         const gameWidth = this.sys.canvas.width;
