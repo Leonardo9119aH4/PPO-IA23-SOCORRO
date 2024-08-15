75951b4f0dbed26edbb8a10c1c2a0845a548435f
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-15 16:10:10 +0000

    sync 2

--- src/api/ranking/getRank.ts
+++ src/api/ranking/getRank.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { Application, Request, Response } from "express";
 
 export async function getRank(app: Application, prisma: PrismaClient){
-    app.post("/api/getrank", async (req: Request, res: Response)=>{
+    app.get("/api/getrank", async (req: Request, res: Response)=>{
         try{
-            let bestUsers: any[]
+            let bestUsers: any[] = []
             const users = await prisma.user.findMany({
                 select: {
                     id: true,
@@ -12,9 +12,23 @@ export async function getRank(app: Application, prisma: PrismaClient){
                     exp: true
                 }
             })
-            users.forEach(el=>{
-                
+            users.forEach((el, i)=>{
+                if(i==0){
+                    for(let x=0; x<5; x++){
+                        bestUsers[x] = el
+                    }
+                }
+                else if(el.exp > bestUsers[0]){
+                    for(let x=4; x>0; x--){
+                        bestUsers[x] = bestUsers[x-1]
+                    }
+                    bestUsers[0] = el
+                }
             })
+            if(users.length<5){ //se a quantidade de usuários for menor que 5, limita a array para não ter usuários multiplicados
+                bestUsers.length = users.length
+            }
+            res.status(200).json(bestUsers)
         }
         catch{
             res.sendStatus(500)
