2477d75623c679902298696f859e2a99b44e54fc
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-16 15:01:42 +0000

    rpg ta saindo

--- src/api/rpg/levels/lv5/level.mjs
+++ src/api/rpg/levels/lv5/level.mjs
@@ -25,49 +25,33 @@ export class Level extends Phaser.Scene {
             frameWidth: 60*26,
             frameHeight: 30*26
         })
-        this.load.spritesheet("bug1Idle", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1Idle.png",{
+        this.load.spritesheet("bug1Idle", "/webSites/rpg/localAssets/sprites/Bug1/Bug1Idle.png",{
             frameWidth: 30*26,
             frameHeight: 30*26
         })
-        this.load.spritesheet("bug1AttackSides", "/webSites/rpg/localAssets/sprites/Bug 1/Bug1AttackSides.png", {
+        this.load.spritesheet("bug1AttackSides", "/webSites/rpg/localAssets/sprites/Bug1/Bug1AttackSides.png", {
             frameWidth: 60*26,
             frameHeight: 30*26
         })
-        this.load.spritesheet("bug2Idle", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2Idle.png", {
+        this.load.spritesheet("bug2Idle", "/webSites/rpg/localAssets/sprites/Bug2/Bug2Idle.png", {
             frameWidth: 30*26,
             frameHeight: 30*26
         })
-        this.load.spritesheet("bug2AttackSides", "/webSites/rpg/localAssets/sprites/Bug 2/Bug2AttackSides.png", {
+        this.load.spritesheet("bug2AttackSides", "/webSites/rpg/localAssets/sprites/Bug2/Bug2AttackSides.png", {
             frameWidth: 60*26,
             frameHeight: 30*26
         })
-        this.load.spritesheet("bug3Idle", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3Idle.png",{
-            frameWidth: 30*26,
-            frameHeight: 30*26
+        this.load.spritesheet("bug3Idle", "/webSites/rpg/localAssets/sprites/Bug3/Bug3Idle.png",{
+            frameWidth: 30*53,
+            frameHeight: 30*53
         })
-        this.load.spritesheet("bug3AttackSides", "/webSites/rpg/localAssets/sprites/Bug 3/Bug3AttackSides.png",{
-            frameWidth: 60*26,
-            frameHeight: 30*26
+        this.load.spritesheet("bug3AttackSides", "/webSites/rpg/localAssets/sprites/Bug3/Bug3AttackSides.png",{
+            frameWidth: 60*53,
+            frameHeight: 30*53
         })
         // this.load.music("main-music", "/webSites/rpg/localAssets/music.mp3")
     }
     create(){
-        // const backgroundMap = this.make.tilemap({ key: 'background-map' })
-        // const backgroundTileset = backgroundMap.addTilesetImage('background-tiles') // Criar o layer de fundo a partir do tilemap
-        // const backgroundLayer = backgroundMap.createLayer('background-tiles', backgroundTileset, 0, 0) // Ajustar o layer de fundo para preencher a tela
-        // backgroundLayer.setScale(2)
-        // backgroundLayer.setOrigin(0, 0)
-        // backgorund.setDisplaySize(534, 401)
-        this.player = this.physics.add.sprite(150, 150, 'playerIdle')
-        this.player.setDisplaySize(150, 150)
-        this.player.setSize(150, 150)
-        this.player.setBounce(0.2)
-        this.player.setCollideWorldBounds(true)
-        this.player.body.setGravity(0, 0)
-        // this.physics.add.collider(this.player.sprite, bug1Idle)
-        // this.physics.add.collider(this.player.sprite, bug2Idle)
-        // this.physics.add.collider(this.player.sprite, bug3Idle)
-        document.addEventListener('executeCode', this.executeCode.bind(this))
         //animações abaixo - Cícero
         this.anims.create({
             key: 'playerIdle', // Nome da animação
@@ -162,16 +146,46 @@ export class Level extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('bug3AttackSides', { start: 0, end: 8 }), // Frames da animação
             frameRate: 8, 
         })
+        //mecânicas e mapa
+        // const backgroundMap = this.make.tilemap({ key: 'background-map' })
+        // const backgroundTileset = backgroundMap.addTilesetImage('background-tiles') // Criar o layer de fundo a partir do tilemap
+        // const backgroundLayer = backgroundMap.createLayer('background-tiles', backgroundTileset, 0, 0) // Ajustar o layer de fundo para preencher a tela
+        // backgroundLayer.setScale(2)
+        // backgroundLayer.setOrigin(0, 0)
+        // backgorund.setDisplaySize(534, 401)
+        this.player = this.physics.add.sprite(150, 150, 'playerIdle')
+        this.player.setDisplaySize(150, 150)
+        this.player.setSize(150, 150)
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
+        this.player.body.setGravity(0, 0)
         this.player.anims.play("playerIdle", true)
-        // const backgorund = this.add.image(0, 0, "backgorund-tiles")
-        // backgorund.setOrigin(0, 0)
-        const wall = this.physics.add.staticGroup()
-        // this.physics.add.collider(this.player.sprite, enemy)
-        //this.physics.add.collider(this.player.sprite, wall)
+        // this.bug1 = this.physics.add.sprite(850, 850, 'bug1Idle')
+        // this.bug2 = this.physics.add.sprite(1050, 1050, 'bug2Idle')
+        this.bug3 = this.physics.add.sprite(650, 650, 'bug3Idle')
+        // this.bug1.setDisplaySize(150, 150)
+        // this.bug1.setSize(150, 150)
+        // this.bug2.setDisplaySize(150, 150)
+        // this.bug2.setSize(150, 150)
+        this.bug3.setDisplaySize(150, 150)
+        this.bug3.setSize(150, 150)
+        // this.bug1.body.setGravity(0, 0)
+        // this.bug2.body.setGravity(0, 0)
+        this.bug3.body.setGravity(0, 0)
+        // this.bug1.anims.play("bug1Idle", true)
+        // this.bug2.anims.play("bug2Idle", true)
+        this.bug3.anims.play("bug3Idle", true)
+        // const wall = this.physics.add.staticGroup()
+        // this.physics.add.collider(this.player.sprite, bug1Idle)
+        // this.physics.add.collider(this.player.sprite, bug2Idle)
+        // this.physics.add.collider(this.player.sprite, bug3Idle)
+        // this.physics.add.collider(this.player.sprite, wall)
+        document.addEventListener('executeCode', this.executeCode.bind(this))
         /* debug */
         this.cursors = this.input.keyboard.createCursorKeys();
         this.SPACEKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
         this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        /* endDebug */
     }
     update(){
         //debug
@@ -193,11 +207,12 @@ export class Level extends Phaser.Scene {
         if(this.SPACEKey.isDown){
             this.player.anims.play("playerIdle", true)
         }
+        //endDebug
     }
     async executeCode(){
         // const actionsRequest = await fetch("/api/private/getExeCode")
         // const actions = await actionsRequest.json()
-        const actions = ["right", "down", "left", "up"]
+        const actions = ["right", "down", "left", "up"] //temporário
         //executor de código
         for(let i=0; i<actions.length; i++){
             if(actions[i]==="up"){
@@ -206,6 +221,7 @@ export class Level extends Phaser.Scene {
                 await new Promise(resolve => setTimeout(()=>{
                     this.player.setVelocityY(0)
                     this.player.anims.play("playerIdle", true)
+                    resolve()
                 }, 1000))
             }
             if(actions[i]==="down"){
@@ -214,6 +230,7 @@ export class Level extends Phaser.Scene {
                 await new Promise(resolve => setTimeout(()=>{
                     this.player.setVelocityY(0)
                     this.player.anims.play("playerIdle", true)
+                    resolve()
                 }, 1000))
             }
             if(actions[i]==="right"){
@@ -222,6 +239,7 @@ export class Level extends Phaser.Scene {
                 await new Promise(resolve => setTimeout(()=>{
                     this.player.setVelocityX(0)
                     this.player.anims.play("playerIdle", true)
+                    resolve()
                 }, 1000))
             }
             if(actions[i]==="left"){
@@ -230,6 +248,7 @@ export class Level extends Phaser.Scene {
                 await new Promise(resolve => setTimeout(()=>{
                     this.player.setVelocityX(0)
                     this.player.anims.play("playerIdle", true)
+                    resolve()
                 }, 1000))
             }
             if(actions[i]==="attack"){
