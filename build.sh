e5b8bd7a58f82a5bb0ac4333415d5e64db478221
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-08-22 16:47:30 -0300

    sync 3

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-# Defina o diretório de origem
-SRC_DIR="./src"
-
 # Compile o TypeScript na mesma pasta
-npx tsc --outDir $SRC_DIR --rootDir $SRC_DIR
+npx tsc 
 
 # Verifique se a compilação foi bem-sucedida
 if [ $? -eq 0 ]; then
     echo "Compilação bem-sucedida. Deletando arquivos .ts..."
 
     # Encontre e delete todos os arquivos .ts na pasta src (exceto os .d.ts, caso existam)
-    find $SRC_DIR -type f -name "*.ts" ! -name "*.d.ts" -delete
+    find $SRC_DIR -type f -name "*.ts" ! -name "*.ts" -delete
 
     echo "Arquivos .ts deletados com sucesso."
 else
