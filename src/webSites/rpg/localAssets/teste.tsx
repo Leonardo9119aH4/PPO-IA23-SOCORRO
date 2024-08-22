17a68504fe7baf20d63c70d3705e651bfec44e90
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-08-22 13:24:59 -0300

    sync 1

--- src/webSites/rpg/localAssets/teste.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="teste" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="/webSites/rpg/localAssets/background-2.png" width="256" height="192"/>
-</tileset>
