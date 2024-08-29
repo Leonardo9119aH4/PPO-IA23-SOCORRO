fa41a2fbc3092658635c96bb95211e0907c76b79
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-29 19:04:55 +0000

    custom nodejs terminal server

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const PORT: number = parseInt(process.env.PORT || '3000')
 const prisma = new PrismaClient()
 
 async function main(){
+  let maxLife: number = 7 //quantidade máxima de vidas
   app.use(bodyParser.json())
   app.use(cookieParser())
   app.set("view engine", "ejs")
@@ -23,10 +24,11 @@ async function main(){
     res.redirect('/webSites/main/index.html');
   })
   app.listen(PORT, () => { // Inicia o servidor
-    console.log(`Servidor iniciado na porta ${PORT}`);
+    process.stdout.write(`Servidor iniciado na porta ${PORT}\n$ `);
   })
-  executeAll(app, prisma, 7) //quantidade máxima de vidas
-  const rl = readline.createInterface({ //comando para fixdb
+  await fixDBErrors(prisma, maxLife) //corrige erros do banco de dados
+  executeAll(app, prisma, maxLife) 
+  const rl = readline.createInterface({ //comando para fixdb a qualquer momento no terminal
     input: process.stdin,
     output: process.stdout,
     terminal: false,
@@ -34,9 +36,21 @@ async function main(){
   rl.on('line', (input: string) => {
     if (input.trim() === 'fixdb') {
       fixDBErrors(prisma, 7);
+      process.stdout.write("Erros do banco de dados corrigidos\n$ ")
+    }
+    else if(input.trim() === 'help' || input.trim() === '?'){
+      process.stdout.write("Lista de comandos disponíveis:\nfixdb: Corrige erros do banco de dados\nexit: Desliga o servidor\nhelp: exibe essa lista\n?: exibe essa lista\nclear: limpa o terminal\ncls: limpa o terminal\n$ ")
+    }
+    else if(input.trim() === 'exit'){
+      prisma.$disconnect().then(()=>{
+        process.exit(1)
+      })
+    }
+    else if(input.trim() === 'clear' || input.trim() === 'cls'){
+      process.stdout.write('\x1Bc\n$ ')
     }
     else {
-      console.log(`Comando desconhecido: ${input}`);
+      process.stdout.write(`Comando desconhecido: ${input}\n$ `);
     }
   });
   
