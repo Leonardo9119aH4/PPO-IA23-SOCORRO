ea954df8df22ff65f27d5b63aa526d25990c18ef
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-08-29 15:08:59 -0300

    29-08

--- src/webSites/blocks/scripts/lv4.mjs
+++ src/webSites/blocks/scripts/lv4.mjs
@@ -1,11 +1,11 @@
 import { Terminal } from "/webSites/blocks/terminal.js"
 export async function Script(terminalElement, dSec, exp){
     let terminal = new Terminal(terminalElement)
-    terminal.write("oi")
-    terminal.write("tchau")
-    let X = parseInt(await terminal.read())
-    let Y = parseInt(await terminal.read())
-    let Z = X+Y
-    terminal.write(Z)
+    let a = 2
+    terminal.write("Digite um valor para b: ")
+    let b = parseInt(await terminal.read())
+    let c = 1 + a + b
+    terminal.write(c)
+    terminal.write(a + b + c)
     terminal.end(dSec, exp)
 }
