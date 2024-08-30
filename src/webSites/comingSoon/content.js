a3ebeb9da975becd9a88f75b1a162413c4fdd78f
Leonardo Artur Sasse Maia Lopes <131017105+Leonardo9119aH4@users.noreply.github.com>
2024-08-30 14:34:49 +0000

    melhorias css

--- src/webSites/comingSoon/content.js
+++ src/webSites/comingSoon/content.js
@@ -1,2 +1,3 @@
 import {main, fatalError} from "/globalAssets/js/main.js"
 main()
+console.log("Se você está vendo isso, saiba que vamos refator todo código e usar EJS direito (ou mudar de framework)")
