2ef556305288fc9e62f116f16777d15b6e1198f7
William1234567891011121314 <williamboeira429@gmail.com>
2024-09-03 21:30:22 -0300

    tela de configurações quase pronta

---
+++ src/webSites/account/assets/security.js
@@ -0,0 +1,25 @@
+export async function loadSecurity() {
+    const apply = document.querySelector("#applyBt")
+    console.log("apply: ", apply)
+    apply.onclick = async function() {
+        let actualPassword = document.querySelector("#actualPassword").value
+        let newPassword = document.querySelector("#newPassword").value
+        let newPasswdConfirm = document.querySelector("#newPasswordConfirmation").value
+        if(newPassword == newPasswdConfirm){
+            let response = await fetch("/api/private/changePassword", {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({actualPassword: actualPassword, newPassword: newPassword})
+            })
+            if(response.status == 201){
+                alert("senha alterada com sucesso!")
+            } else {
+                alert("Senha atual incorreta!")
+            }
+        } else {
+            alert("A sua nova senha e a confirmação são diferentes!")
+        }
+    }
+}
--- src/webSites/account/content.js
+++ src/webSites/account/content.js
@@ -1,11 +1,32 @@
 import {main} from '/globalAssets/js/main.js'
 main()
-const graphic = document.querySelector("div#graphic")
-var previousvacon = 0, cont = 0, xlayerunits = 7, ylayerunits = 10 , scale = 1000;
+import { loadSecurity } from '/webSites/account/assets/security.js'
+import { loadPersonalInformations } from '/webSites/account/assets/personalInformations.js'
 
-graphic.style.gridTemplateColumns = `repeat(${xlayerunits}, 1fr)`;
-graphic.style.gridTemplateRows = `repeat(${ylayerunits}, 1fr)`;
-
-for(let i = 0; i < xlayerunits * ylayerunits; i++) {
-    graphic.innerHTML += "<span></span>";
+async function getHtml(htmlName){
+    let htmlOption = await fetch(`/websites/account/assets/${htmlName}.html`)
+    return htmlOption.text()
 }
+
+const options = document.querySelectorAll("#options button")
+
+options.forEach(option => {
+    option.addEventListener("click", async () => {
+        options.forEach(option => {
+            option.style.backgroundColor = "transparent"
+        })
+        option.style.backgroundColor = "#ffffff"
+        document.querySelector("#sectionContent").innerHTML = await getHtml(option.id)
+        document.querySelector("#sectionName").innerHTML = option.querySelector("p").innerHTML
+        //eu me odeio por fazer isso mas tenho que terminar logo
+        switch (option.id){
+            case "security":
+                loadSecurity()
+                break
+            case "personalInformations":
+                loadPersonalInformations()
+        }
+    })
+})
+
+document.querySelector("#security").click()
