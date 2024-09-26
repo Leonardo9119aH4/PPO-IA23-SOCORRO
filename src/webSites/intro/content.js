b550c92e7bef5064e4c6cde9cbe5ede2211da489
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-09-26 13:31:56 -0300

    sync 2

--- src/webSites/intro/content.js
+++ src/webSites/intro/content.js
@@ -1,11 +1,6 @@
 import {main} from "/globalAssets/js/main/js"
-const mainTag = document.querySelector("main")
-const contBt = mainTag.querySelector("button")
-
 async function content(){
-    await main()
-    contBt.addEventListener("click", ev =>{
-        
-    })
+    
 }
+main()
 content()
